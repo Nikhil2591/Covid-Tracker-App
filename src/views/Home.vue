5eb70a0d476b400636968fbcd56707e81bfcb66a
Nikhil2591 <Nikhil_369@hotmail.co.uk>
2021-05-10 15:15:30 +0100

    First Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello World
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {},
+  methods: {
+    async fetchCovidData(){
+      const res = await fetch('')
+    },
+    
+  },
+  created() {
+    
   }
 }
 </script>
